0bec6bf5caa896c67f4eb0d40d67d849cf8c5960
horaemong <jaeho3836@gmail.com>
2022-12-12 22:26:50 +0900

    최적화

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import time
 import pyautogui  
 import pyperclip 
 import datetime
+import os
 
 def open_app():
     try:
@@ -14,24 +15,38 @@ def open_app():
     except:
         print("cannot open KakaoTalk")
         
-def login_app():
-    button_location = pyautogui.locateOnScreen('images/login_password.png', confidence=0.9)
-    button_location_2 = pyautogui.locateOnScreen('images/login_login.png', confidence=0.9)
+def login_app(password):
+    button_location = pyautogui.locateOnScreen('login_password.png', confidence=0.9)
+    button_location_2 = pyautogui.locateOnScreen('login_login.png', confidence=0.9)
     if button_location is None and button_location_2 is None:
         print("패스워드 버튼 찾기 실패 ㅠㅠ")
     elif button_location is not None:
         button_point = pyautogui.center(button_location)
         pyautogui.click(button_point.x, button_point.y)
-        pyautogui.write('Password1234') 
+        pyautogui.write(password) 
         pyautogui.press('enter')
     elif button_location_2 is not None:
         button_point = pyautogui.center(button_location_2)
 #     pyautogui.moveTo(button_point.x, button_point.y, duration=0)
 #     pyautogui.move(0, -45, 0.5, pyautogui.easeInQuad)  # Move 45 pixels Up
         pyautogui.doubleClick(button_point.x, button_point.y-45)
-        pyautogui.write('Password1234')
+        pyautogui.write(password)
         pyautogui.press('enter')
    
    
-open_app()        
-login_app()
+
+open_app()
+
+#창이 열렸는지 확인
+win = ''
+while not win:
+    try:
+        win = pyautogui.getWindowsWithTitle('카카오톡')[0]
+    except:
+        print("카카오톡 창을 찾을 수 없습니다.")
+        time.sleep(1)
+
+login_app('비밀번호')
+
+
+#pyinstaller app.py
